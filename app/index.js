751914234a9b91d6dfdef73ba7b4e38ff66983a4
adrizzo <zplaguee@gmail.com>
2020-12-16 17:04:48 -0500

    Build production & redirects

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
-import Results from './components/Results'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Loading from './components/Loading'
+
+// Dynamic imports
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 class App extends React.Component {
   constructor(props) {
@@ -28,12 +31,14 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className='container'>
               <Nav />
-              <Switch>
-                <Route exact path='/' component={Popular} />
-                <Route exact path='/battle' component={Battle} />
-                <Route path='/battle/results' component={Results} />
-                <Route render={() => <h1>404</h1>}></Route>
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path='/' component={Popular} />
+                  <Route exact path='/battle' component={Battle} />
+                  <Route path='/battle/results' component={Results} />
+                  <Route render={() => <h1>404</h1>}></Route>
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
@@ -18,10 +19,13 @@ module.exports = {
     contentBase: './dist',
     historyApiFallback: true,
   },
-  mode: 'development',
+
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyPlugin({ patterns: [{ from : '_redirects' }] })
   ]
 }
